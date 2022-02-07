da643ef861782d8ca1718e13f66e751afe17f901
Mario <oiram.251170@gmail.com>
2022-02-07 15:49:28 -0600

    Lista reactiva con datos

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -3,14 +3,22 @@
     <h3>Main</h3>
     <div class="container">
       <div class="card">
-        <img id="imgCard" v-bind:src="imagen" alt="Imagen" />
+        <img id="imgCard" v-bind:src="pokemon.imagen" alt="Imagen" />
         <div class="card-body">
-          <h5 class="card-title">{{ nombre }}</h5>
+          <h5 class="card-title">{{ pokemon.nombre }}</h5>
         </div>
-        <div v-if="evol" class="card-footer">
+        <div v-if="pokemon.evol.evoluciona" class="card-footer">
           <i class="fas fa-angle-double-up"></i>
-          <img v-bind:src="imagenEvol" alt="Imagen" />
-          <p class="card-text">{{ nombreEvol }}</p>
+          <img v-bind:src="pokemon.evol.imagen" alt="Imagen" />
+          <p class="card-text">{{ pokemon.evol.nombre }}</p>
+        </div>
+        <div class="card-footer">
+          <h5>Ataques:</h5>
+          <ul>
+            <li v-for="ataque in pokemon.ataques" :key="ataque.id">
+              {{ ataque }}
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -18,23 +26,24 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive } from "vue";
 import pikachu from "../assets/pokemons/pikachu.png";
 import raichu from "../assets/pokemons/raichu.png";
 
 export default {
   setup() {
-    const nombre = ref("Pikachu");
-    const imagen = pikachu;
-    const evol = true;
-    const imagenEvol = raichu;
-    const nombreEvol = ref("Raichu");
+    const pokemon = reactive({
+      nombre: "Pikachu",
+      imagen: pikachu,
+      evol: {
+        evoluciona: true,
+        imagen: raichu,
+        nombre: "Raichu",
+      },
+      ataques: ["Trueno", "Chispa", "Agilidad", "Rayo"],
+    });
     return {
-      nombre,
-      imagen,
-      evol,
-      imagenEvol,
-      nombreEvol,
+      pokemon,
     };
   },
 };
